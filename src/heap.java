b4478aa60652acd63ee05efcb2caeb7accde5b09
Madhur kumar singh <madhursingh04072@gmail.com>
2022-04-25 11:41:15 +0530

    after comments

--- src/heap.java
+++ src/heap.java
@@ -6,22 +6,27 @@ public class heap {
         al=new ArrayList<>();
 
     }
+    //to find size
     public int size(){
         return al.size();
     }
+    //to find it is empty or not
     public boolean isEmpty(){
         return al.size()==0;
     }
+    \//to display
     public void display(){
         System.out.println(al);
     }
+    //to display only first element as queue show first in first out
     public int peek(){
         return al.get(0);
-    }
+    }// to add in priority queue
     public void add(int ali){
         al.add(ali);
         upHeapify(al.size()-1);
     }
+    // for checking the condition for heapify
     private void upHeapify(int child){
         int parent=(child-1)/2;
         if(al.get(parent)>al.get(child))
@@ -31,6 +36,7 @@ public class heap {
         }
 
     }
+    // for swaping
     private void swap(int child,int parent){
         int temp=al.get(child);
         al.set(child,al.get(parent));
